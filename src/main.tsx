68e38c4298757c51cc390545025d5df6f4f89128
youssefmd2244-droid <youssefmd2244@gmail.com>
2026-07-01 21:50:38 +0300

    Wrap App component in ErrorBoundary

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { ErrorBoundary } from "./components/ErrorBoundary";
 import { bootstrapCapacitor } from "./mobile/capacitor";
 
 bootstrapCapacitor();
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <ErrorBoundary>
+      <App />
+    </ErrorBoundary>
   </StrictMode>
 );
